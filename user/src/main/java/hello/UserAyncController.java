93cc47e46ab42850be2864c7a5c1340d3f998cfd
flydrago <1162040314@qq.com>
2024-03-12 17:34:21 +0800

    Remove all files from Git tracking

--- user/src/main/java/hello/UserAyncController.java
+++ user/src/main/java/hello/UserAyncController.java
@@ -33,4 +33,19 @@ public class UserAyncController {
 
         return responseMono;
     }
+
+    @RequestMapping("/async/hello")
+    public Mono<String> hello(@RequestParam(value = "name", defaultValue = "Mary") String name) {
+         // 使用webClientBuilder构建WebClient实例
+        WebClient webClient = webClientBuilder.build();
+        
+        // 发起GET请求，并处理响应
+        Mono<String> responseMono = webClient.get()
+            .uri(SAY_HELLO_ENDPOINT + "/greeting") // 设置请求URI
+            .retrieve() // 获取响应体
+            .bodyToMono(String.class)
+            .map(greeting -> String.format("%s, %s!", greeting, name));
+
+        return responseMono;
+    }
 }
